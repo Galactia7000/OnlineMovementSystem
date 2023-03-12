05f1db7967c13c0d4e3558db553ee7257f5f0009
Peter Lloyd <Galactia7000@gmail.com>
2023-03-12 18:09:47 +0000

    Add project files.

---
+++ MovementServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace MovementServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
